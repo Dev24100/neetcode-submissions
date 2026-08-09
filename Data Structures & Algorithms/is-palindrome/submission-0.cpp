d23860249125525e07d083a532333bc2f021d2ad
Dev Shah <218080654+Dev24100@users.noreply.github.com>
2026-08-09 23:30:37 +0530

    Add comment for palindrome function clarity
    Added a comment explaining the palindrome definition and function purpose.

--- Data Structures & Algorithms/is-palindrome/submission-0.cpp
+++ Data Structures & Algorithms/is-palindrome/submission-0.cpp
@@ -1,4 +1,8 @@
-class Solution {
+/*  Given a string s, return true if it is a palindrome, otherwise return false. */
+
+A palindrome is a string that reads the same forward and backward. It is also case-insensitive and ignores all non-alphanumeric characters.
+    
+    class Solution {
 public:
     bool isPalindrome(string s) {
 
