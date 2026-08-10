4a412146430a266adc17931345c6a9547891359b
Dev Shah <218080654+Dev24100@users.noreply.github.com>
2026-08-11 02:12:31 +0530

    Add: is-subsequence - submission-0

---
+++ Data Structures & Algorithms/is-subsequence/submission-0.cpp
@@ -0,0 +1,19 @@
+#include <string>
+
+class Solution {
+public:
+    bool isSubsequence(std::string s, std::string t) {
+        int i = 0; // Pointer for string s
+        int j = 0; // Pointer for string t
+
+        while (i < s.length() && j < t.length()) {
+            if (s[i] == t[j]) {
+                i++;
+            }
+            j++; // Always move pointer for t
+        }
+
+      
+        return i == s.length();
+    }
+};
